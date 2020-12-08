0092e9cbde8e96c1c3cc06f8566f57278e0539c5
unknown <jupiter85838@yahoo.com>
2020-12-08 09:15:47 -0500

    add eureka server, update resource servers to be client, there is an issue with spring version, i need to downgrade from spring boot 2.4.0

--- album-resource-server-app/src/main/java/com/album/server/AlbumResourceServerAppApplication.java
+++ album-resource-server-app/src/main/java/com/album/server/AlbumResourceServerAppApplication.java
@@ -2,8 +2,10 @@ package com.album.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AlbumResourceServerAppApplication {
 
 	public static void main(String[] args) {
--- resource-server-app/src/main/java/com/resourceserver/ResourceServerAppApplication.java
+++ resource-server-app/src/main/java/com/resourceserver/ResourceServerAppApplication.java
@@ -2,8 +2,16 @@ package com.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+
+//@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
+//@EnableDiscoveryClient
+
 public class ResourceServerAppApplication {
 
 	public static void main(String[] args) {
